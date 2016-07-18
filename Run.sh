4ab2889be37c587e84b74e271665fd54a7c09306
= <=>
2016-07-17 21:15:10 -0700

    Server Results

--- Run.sh
+++ Run.sh
@@ -1,24 +1,24 @@
-python3 Main.py ScriptClientBoltzman.sh dataTransfer
+python3 Main.py ScriptClientBoltzman.sh Serverondemand
 echo Complete
-python3 Main.py ScriptClientBayesNetwork.sh dataTransfer
+python3 Main.py ScriptClientBayesNetwork.sh Serverondemand
 echo complete
-python3 Main.py ScriptClientCostAssignment.sh dataTransfer
+python3 Main.py ScriptClientCostAssignment.sh Serverondemand
 echo Complete
-#python3 Main.py ScriptClientFaceDetector.sh dataTransfer
+#python3 Main.py ScriptClientFaceDetector.sh Serverondemand
 echo Complete
-python3 Main.py ScriptClientFileSearch.sh dataTransfer
+python3 Main.py ScriptClientFileSearch.sh Serverondemand
 echo Complete
-python3 Main.py ScriptClientKmean.sh dataTransfer
+python3 Main.py ScriptClientKmean.sh Serverondemand
 echo Complete
-python3 Main.py ScriptClientRunningStatistics.sh dataTransfer
+python3 Main.py ScriptClientRunningStatistics.sh Serverondemand
 echo Complete
-python3 Main.py ScriptClientSort.sh dataTransfer
+python3 Main.py ScriptClientSort.sh Serverondemand
 echo Complete
-python3 Main.py ScriptClientSvmPegasos.sh dataTransfer
+python3 Main.py ScriptClientSvmPegasos.sh Serverondemand
 echo Complete
-python3 Main.py ScriptClientSvmRank.sh dataTransfer
+python3 Main.py ScriptClientSvmRank.sh Serverondemand
 echo Complete
-#python3 Main.py ScriptClientVideoTrack.sh dataTransfer
+#python3 Main.py ScriptClientVideoTrack.sh Serverondemand
 echo Complete
 
 
--- ScriptClientBayesNetwork.sh~
+++ ScriptClientBayesNetwork.sh~
@@ -1,7 +1,7 @@
 scp ~/Softwares/dlib-18.18/examples/bayes_net_ex.cpp rshukla@10.42.43.1:/home/rshukla/Softwares/dlib-18.18/examples
 scp BayesNetwork.sh rshukla@10.42.43.1:/home/rshukla/OffloadedJobs
 
-ssh  rshukla@10.42.43.1 "cd OffloadedJobs && chmod +x BayesNetwork.sh.sh && /usr/bin/time -a  -o StatisticsBayesNetwork.csv -f "%S","%U",%P,%F,%R ./BayesNetwork.sh"
+ssh  rshukla@10.42.43.1 "cd OffloadedJobs && chmod +x BayesNetwork.sh && /usr/bin/time -a  -o StatisticsBayesNetwork.csv -f "%S","%U",%P,%F,%R ./BayesNetwork.sh"
 
 scp rshukla@10.42.43.1:/home/rshukla/Softwares/dlib-18.18/examples/outFileBayesNetwork ~/ClientServerCode
 
